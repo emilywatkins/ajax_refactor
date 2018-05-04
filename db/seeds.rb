09207bcd0ab975b681236c6b28fe4d9a4d62aeda
Emily Watkins <emily.e.dolliver@gmail.com>
2018-05-04 16:47:53 -0700

    add images column, add faker for image, add to div, encapsulate toggle, working on default hide.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@ class Seed
   end
 
   def generate_products
+    Product.destroy_all
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop, image: Faker::Fillmurray.image)
     end
   end
 end
