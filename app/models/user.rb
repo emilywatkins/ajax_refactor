8e5766001fd9269cad0ad81dee39c68ee0752d0b
Emily Watkins <emily.e.dolliver@gmail.com>
2018-05-06 13:48:31 -0700

    add validations for products.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :orders
 
   validates :password, length: { minimum: 8 }
+  # validates :password, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)\z/ }
 
   def previous_orders
     self.orders.where(status: 2).includes(order_items: :product)
